0b5ac99c4722d444e346b06161db093a51b973e9
ManasRaj07 <manasraj831@gmail.com>
2026-05-03 13:27:42 +0530

    Set base path for homeo-hive-website

--- vite.config.ts
+++ vite.config.ts
@@ -5,6 +5,7 @@ import { componentTagger } from "lovable-tagger";
 
 // https://vitejs.dev/config/
 export default defineConfig(({ mode }) => ({
+  base: "/homeo-hive-website/", 
   server: {
     host: "::",
     port: 8080,
